65b31cc5cea5f5edaef6d88e1201c0c8d29db9a5
Christian Pinto <pintochristian2@gmail.com>
2019-04-21 01:22:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word(string)
   #binding.pry
  strings = string.split(' ')
  reversed = []
- strings.each {|char| reversed.unshift(char)}
+ strings.collect {|char| reversed.unshift(char)}
  return reversed.join(" ")
 end
